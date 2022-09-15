8657a3eed24e9df7ea4c70c02572bfe80ef072ff
RN-VINder <75273407+RN-VINder@users.noreply.github.com>
2022-09-15 21:44:41 +0200

    Update datatables.js

--- datatables.js
+++ datatables.js
@@ -26,12 +26,12 @@ $(document).ready(function () {
 			language: {
 									url: 'https://cdn.datatables.net/plug-ins/1.12.1/i18n/de-DE.json'
 								},
-	
+	serverSide: true,
 		                processing: true,
         
         ajax: {
             url: 'https://opensheet.elk.sh/1OWLHbaylVdtpe69iHDSubjp4hhBv78WzCt10wzd9psw/1',
-            dataType: '',
+            dataSrc: '',
         },
 		
 		
