cd01fcdd92daba8238ab00f8084b73a857507cb8
RN-VINder <75273407+RN-VINder@users.noreply.github.com>
2022-09-24 02:58:53 +0200

    Update datatables.js

--- datatables.js
+++ datatables.js
@@ -1,142 +1,108 @@
-
-// nehme URLS
-var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = window.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
-
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-        }
-    }
-    return false;
-};
-
-var spaeter = getUrlParameter('spaeter');
-
-
-
-
 $(document).ready(function () {
-	var table = $('#datatable').DataTable({
-			language: {
-									url: 'https://cdn.datatables.net/plug-ins/1.12.1/i18n/de-DE.json'
-								},	             
+    var table = $('#table').DataTable({
+  			language: {url: 'https://cdn.datatables.net/plug-ins/1.12.1/i18n/de-DE.json'},
         ajax: {
-            url: 'https://opensheet.elk.sh/1OWLHbaylVdtpe69iHDSubjp4hhBv78WzCt10wzd9psw/1',
-            dataSrc: '',
-        },		
-      dom: 'PQlfrtip',
-      columns: [
-                  {
-                  data: "",
-                  defaultContent: "",
+            url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ2IqZXswF4hLOmVsRUov5O8Fz-4KjSCqs3KRHw4EKLowog0U0vYjl3rHvN6ajVdiXFokqdlTesgJZD/pub?gid=0&single=true&output=csv",
+      			dataType: 'text',
+      			dataSrc: function (csvdata) {
+          		var data = csvdata.split(/\r?\n|\r/);
+              return $.csv.toObjects(csvdata);
+            }
+          },//end ajax load
+      	processing: true,
+        scrollY: 400,
+        deferRender: true,
+        scroller: {
+        		displayBuffer: 5
+   				},
+        columns: [
+            {
+                data: "",
+                defaultContent: "",
                 searchable: false,
                 orderable: false,
-                className: '',
+                className: 'dtr-control',
             },
-{ data: 'Zeitstempel', searchable: false},
-{ data: 'TTF Username',          className: ''},
-{ data: 'Bestelldatum' },
-{ data: 'Bestellnummer' },
-{ data: 'Bestellung geschoben?' },
-{ data: 'Außenfarbe (Lack)' },
-{ data: 'Innenraum' },
-{ data: 'Model' },
-{ data: 'Variante' },
-{ data: 'Felgen' },
-{ data: 'Autopilot' },
-{ data: 'Anhängerkupplung' },
-{ data: 'Winterreifen' },
-{ data: 'Lieferstatus' },
-{ data: 'Liefermonat' },
-{ data: 'Lieferzeitraum START' },
-{ data: 'Lieferzeitraum ENDE' },
-{ data: 'Auslieferungsland' },
-{ data: 'ALZ - Deutschland' },
-{ data: 'ALZ - Österreich' },
-{ data: 'ALZ - Schweiz' },
-{ data: 'ALZ - Sonstiges' },
-{ data: 'VIN-Zuteilung' },
-{ data: 'Fin-Vin' },
-{ data: 'Herstellungsdatum' },
-{ data: 'Übergabedatum (geplant)' },
-{ data: 'Übergabedatum (tatsächlich)' },
-{ data: 'Zulassung' },
-{ data: 'BAFA Antrag' },
-{ data: 'BAFA Bewilligung' },
-{ data: 'BAFA Auszahlung' },
-{ data: 'Kommentare / Zusatzinformationen' },
-      ],
-   "processing": true,
-    responsive: {
-        details: {
-										target: 'tr'
-                }
-    },
-    
-   "searching": true,
-   "scrollY": 600,
-   "paging":   false,
-   "ordering": false,
-   "order": [[3, 'desc']],
-   
-   "info":     true,
-    				"searchPanes": {
+            { data: 'Zeitstempel', searchable: false},
+            { data: 'TTF Username', width: '10px', className: 'ui table'},
+            { data: 'Bestelldatum', type: 'date-eu' },
+            { data: 'Bestellnummer' },
+            { data: 'Bestellung geschoben?' },
+            { data: 'Außenfarbe (Lack)', className:'truncate' },
+            { data: 'Innenraum', className:'truncate' },
+            { data: 'Model' },
+            { data: 'Variante' },
+            { data: 'Felgen', className:'truncate' },
+            { data: 'Autopilot' },
+            { data: 'Anhängerkupplung' },
+            { data: 'Winterreifen' },
+            { data: 'Lieferstatus' },
+            { data: 'Liefermonat' },
+            { data: 'Lieferzeitraum START' },
+            { data: 'Lieferzeitraum ENDE' },
+            { data: 'Auslieferungsland' },
+            { data: 'ALZ - Deutschland', className:'truncate' },
+            { data: 'ALZ - Österreich' },
+            { data: 'ALZ - Schweiz' },
+            { data: 'ALZ - Sonstiges' },
+            { data: 'VIN-Zuteilung' },
+            { data: 'VIN' },
+            { data: 'Herstellungsdatum', type: 'date-eu' },
+            { data: 'Übergabedatum (geplant)', type: 'date-eu' },
+            { data: 'Übergabedatum (tatsächlich)', type: 'date-eu' },
+            { data: 'Zulassung', type: 'date-eu' },
+            { data: 'BAFA Antrag', type: 'date-eu' },
+            { data: 'BAFA Bewilligung', type: 'date-eu' },
+            { data: 'BAFA Auszahlung', type: 'date-eu' },
+            { data: 'Kommentare / Zusatzinformationen' },
+      		],
+			columnDefs: [
+						{ "defaultContent": "", "targets": "_all" },
+            { responsivePriority: 1, targets: [0,2,3,4,6,7,10,16,17,19,23,24] },
+						{ responsivePriority: 10001, targets: [1, 5, 8, 9, 11, 12, 13, 14, 15, 18, 20, 21, 22, 25, 26, 27, 28, 29, 30, 31, 32] },
+					],
+      responsive: {
+            details: {
+                renderer: function(api, rowIdx, columns){
+                    let render_method = $.fn.dataTable.Responsive.renderer.tableAll({tableClass: 'ui table'})
+                    return render_method(api, rowIdx, columns.filter(column => column.hidden && column.data))
+                  }
+              }
+  			},
+      searchPanes: {
 						controls: false,
 						columns: [19,8,9,6,7,10],
-                              panes: [
+            	panes: [
                 {
-                    header: 'VIN-Vergabe',
+                    header: 'VIN zugeteilt?',
                     options: [
-                        {
-                            label: 'Hat bisher keine VIN-Zuteilung bzw. Abschlussrechnung',
+                                                {
+                            label: 'Hat eine VIN-Zuteilung bzw. Abschlussrechnung',
                             value: function(rowData, rowIdx) {
-                                return rowData['VIN-Zuteilung'] === '' || rowData['Fin-Vin'] === '';
+                                return rowData['.VIN'] !== '' || rowData['VIN-Zuteilung'] !== '';
                             }
                         },
                         {
-                            label: 'Hat eine VIN-Zuteilung bzw. Abschlussrechnung',
+                            label: 'Hat bisher keine VIN-Zuteilung bzw. Abschlussrechnung',
                             value: function(rowData, rowIdx) {
-                                return rowData['VIN-Zuteilung'] !== '' || rowData['Fin-Vin'] !== '';
-                            }
+                                return rowData['.VIN'] === '' || rowData['VIN-Zuteilung'] === '';
+                                    }
+                                }
+
+                            ],
+
                         }
-                        
                     ],
-
-                }
-            ],
-						cascadePanes: false,
-            dtOpts: {
-                select: { style: 'multi' }
-                }
-				},
-      //dom: 'PQlfrtip',
-			stateSave: false,
-	columnDefs: [
-					{ "defaultContent": "", "targets": "_all" },
-                  //  { "visible": false, "targets": [] },
-            
-{ responsivePriority: 1, targets: [0,2,3,4,6,7,10,16,17,19] },
-{ responsivePriority: 10001, targets: [1, 5, 8, 9, 11, 12, 13, 14, 15, 18, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32] },
-                  
-					//{ "visible": false, "targets": [3,4,10,11,12,13,14,17,19,20,21,24,25,26,27,28,29,30,31] },
-
-/*
-					{ searchPanes: { show: true }, targets: [22] },
-					{ searchPanes: { show: false }, targets: [0,3,4,10,11,12,13,14,17,19,20,23,24,25,26,27,28,29,30,31] }
-*/
-
-          ]
-                  
-
-});
-// end loadready
-
+                    cascadePanes: false,
+                    dtOpts: {select: {style: 'multi'} }
+					},
+    	dom: 'PQlfrtip',
+    	order: [[3, 'asc']],
+    
+   // End Datatables 
+   });
+    
+//add posiition numbers
     table.on('order.dt search.dt', function () {
         let i = 1;
  
@@ -145,7 +111,6 @@ $(document).ready(function () {
         });
     }).draw();
 
-    
 
-});
+})
 
